c4e287bfbf2e61d54413f214202ef7634e9c0f73
manuthecoder <manusvathgurudath@gmail.com>
2022-08-25 16:33:54 +0000

    docs: update README.md

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+    </>
+  );
+}
