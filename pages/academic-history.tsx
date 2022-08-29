ab006bb93bceb6f16cfd3724222302e3234a113e
manuthecoder <manusvathgurudath@gmail.com>
2022-08-29 09:49:24 -0700

    Create academic-history.tsx

---
+++ pages/academic-history.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Head from "next/head";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActionArea from "@mui/material/CardActionArea";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { cyan, green } from "@mui/material/colors";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <Box sx={{ p: { xs: 3, sm: 7 } }}>
+        <Box
+          sx={{
+            "@keyframes this": {
+              "0%": {
+                boxShadow: "0 0 30px " + cyan["900"],
+              },
+              "50%": {
+                boxShadow: "0 0 50px " + cyan["900"],
+              },
+              "100%": {
+                boxShadow: "0 0 30px " + cyan["900"],
+              },
+            },
+            p: 5,
+            mt: 2,
+            py: 8,
+            textAlign: "center",
+            backdropFilter: "blur(10px)",
+            borderRadius: 5,
+            border: "2px solid " + cyan["A700"],
+            // transition: "box-shadow .5s",
+            animation: "this 5s infinite",
+            color: cyan["A400"],
+          }}
+        >
+          <Typography variant="h2" sx={{ fontWeight: "800" }}>
+            Academic history
+          </Typography>
+        </Box>
+        <Box sx={{ maxWidth: "800px", pt: 4 }}>
+          <Typography sx={{ mb: 2, mt: 1 }}>
+            <b>Tell?</b>
+            <br />I was born in India. I love the food there, because it is very
+            unique and delicious.
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
+}
